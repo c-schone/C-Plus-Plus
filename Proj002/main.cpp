8a78d402cf915f11bfc5bc5cd5c0850354d37fea
Christian Schone <cschone@novomind.com>
2025-11-07 18:39:33 +0100

    Initial Commit for C++ learn repository

---
+++ Proj002/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int fav_number;
+  cout << "Hello from Proj002" << endl;
+  cout << "Enter your favorite number: ";
+  cin >> fav_number;
+  cout << "Your favorite number is " << fav_number << endl;
+  return 0;
+}
